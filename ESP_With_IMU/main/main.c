4fc185eebf6cc2ab78cd02c420ab250f2f994a65
Sanim Kumar Khatri <72692293+SanimKumarKhatri@users.noreply.github.com>
2025-02-21 01:16:44 +0545

    updated the main app to integrate teh mpu6050 initialization

--- ESP_With_IMU/main/main.c
+++ ESP_With_IMU/main/main.c
@@ -1,44 +1,26 @@
-/* i2c - Simple example
-
-   Simple I2C example that shows how to initialize I2C
-   as well as reading and writing from and to registers for a sensor connected over I2C.
-
-   The sensor used in this example is a MPU9250 inertial measurement unit.
-
-   For other examples please check:
-   https://github.com/espressif/esp-idf/tree/master/examples
-
-   See README.md file to get detailed usage of this example.
-
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
 #include <stdio.h>
 #include "esp_log.h"
 #include "driver/i2c.h"
 #include "settings.h"
 #include "wifi_app.h"
+#include "mpu6050.h"
 
 static const char *TAG = "main";
 
 void app_main(void)
 {
     settings_init();
-    uint8_t data[2];
-    // ESP_ERROR_CHECK(i2c_master_init());
-    ESP_LOGI(TAG, "I2C initialized successfully");
     wifi_app_init();
+    mpu6050_init(0, 0, true);
+
+    uint8_t data[2];
+
+    ESP_LOGI(TAG, "I2C initialized successfully");
+    
     ESP_LOGI(TAG, "helloo!");
+
     /* Read the MPU9250 WHO_AM_I register, on power up the register should have the value 0x71 */
     // ESP_ERROR_CHECK(mpu9250_register_read(MPU9250_WHO_AM_I_REG_ADDR, data, 1));
-    // ESP_LOGI(TAG, "WHO_AM_I = %X", data[0]);
+    ESP_LOGI(TAG, "ACCEL_X= %0.2f", mpu6050_get_accel_x());
 
-    // /* Demonstrate writing by reseting the MPU9250 */
-    // ESP_ERROR_CHECK(mpu9250_register_write_byte(MPU9250_PWR_MGMT_1_REG_ADDR, 1 << MPU9250_RESET_BIT));
-
-    // ESP_ERROR_CHECK(i2c_driver_delete(I2C_MASTER_NUM));
-    ESP_LOGI(TAG, "I2C de-initialized successfully");
 }
