0f85d5ee6f037f7625ecd795ffe0a1b142c3b3aa
SanimKumarKhatri <khatri.sanim@gmail.com>
2025-02-21 11:48:47 +0545

    added webserver to teh main app

--- ESP_With_IMU/main/main.c
+++ ESP_With_IMU/main/main.c
@@ -4,6 +4,7 @@
 #include "settings.h"
 #include "wifi_app.h"
 #include "mpu6050.h"
+#include "webserver.h"
 
 static const char *TAG = "main";
 
@@ -11,6 +12,7 @@ void app_main(void)
 {
     settings_init();
     wifi_app_init();
+    sensor_webpage_init();
     mpu6050_init(MPU6050_ACCEL_RANGE_2G, MPU6050_GYRO_RANGE_250, true);
     mpu6050_set_filter_bandwidth(MPU6050_BAND_21_HZ);
     uint8_t data[2];
