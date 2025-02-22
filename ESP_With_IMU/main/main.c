1de64683ce37ec09aae8d89fdcf46f22028ad99a
Sanim Kumar Khatri <72692293+SanimKumarKhatri@users.noreply.github.com>
2025-02-22 18:11:40 +0545

    made the code to read and publish data every 10 seconds

--- ESP_With_IMU/main/main.c
+++ ESP_With_IMU/main/main.c
@@ -5,6 +5,7 @@
 #include "wifi_app.h"
 #include "mpu6050.h"
 #include "webserver.h"
+#include "mqtt.h"
 
 static const char *TAG = "main";
 
@@ -15,15 +16,16 @@ void app_main(void)
     sensor_webpage_init();
     mpu6050_init(MPU6050_ACCEL_RANGE_2G, MPU6050_GYRO_RANGE_250, true);
     mpu6050_set_filter_bandwidth(MPU6050_BAND_21_HZ);
-    uint8_t data[2];
-
     ESP_LOGI(TAG, "I2C initialized successfully");
     
     ESP_LOGI(TAG, "helloo!");
 
-    /* Read the MPU9250 WHO_AM_I register, on power up the register should have the value 0x71 */
-    // ESP_ERROR_CHECK(mpu9250_register_read(MPU9250_WHO_AM_I_REG_ADDR, data, 1));
-    ESP_LOGI(TAG, "ACCEL_X= %0.2f", mpu6050_get_accel_x());
-    ESP_LOGI(TAG, "ACCEL_Y= %0.2f", mpu6050_get_accel_y());
+    while (1)
+    {
+        ESP_LOGI(TAG, "ACCEL_X= %0.2f", mpu6050_get_accel_x());
+        ESP_LOGI(TAG, "ACCEL_Y= %0.2f", mpu6050_get_accel_y());
+        mqtt_publish_data(mpu6050_get_accel_x(), mpu6050_get_accel_y(), mpu6050_get_accel_z());
+        vTaskDelay(10000);
+    }
 
 }
