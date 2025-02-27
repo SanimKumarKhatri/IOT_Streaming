1e467485e1e143063bf4a8739abf870cfc63bdef
SanimKumarKhatri <khatri.sanim@gmail.com>
2025-02-27 13:05:42 +0545

    changes to main to accomodate datetime data

--- ESP_With_IMU/main/main.c
+++ ESP_With_IMU/main/main.c
@@ -6,9 +6,10 @@
 #include "mpu6050.h"
 #include "webserver.h"
 #include "mqtt.h"
+#include "sntp_app.h"
 
 static const char *TAG = "main";
-
+static char datetime[64];
 static void _mqtt_sensor_task(void *p_arg);
 
 void app_main(void)
@@ -19,7 +20,6 @@ void app_main(void)
     mpu6050_init(MPU6050_ACCEL_RANGE_2G, MPU6050_GYRO_RANGE_250, true);
     mpu6050_set_filter_bandwidth(MPU6050_BAND_21_HZ);
     ESP_LOGI(TAG, "I2C initialized successfully");
-
     xTaskCreate(_mqtt_sensor_task, "pub_task", 2 * 1024, NULL, 1, NULL);
 }
 
@@ -29,10 +29,16 @@ static void _mqtt_sensor_task (void * p_arg)
 
     while (1)
     {
+        if (1 == wifi_app_wait_for_station_connection(portMAX_DELAY))
+        {
+            sntp_get_current_time(&datetime[0]);
+            mqtt_publish_data (&datetime[0], mpu6050_get_accel_x(), mpu6050_get_accel_y(), mpu6050_get_accel_z(),
+            mpu6050_get_gyro_x(), mpu6050_get_gyro_y(), mpu6050_get_gyro_z(), mpu6050_get_temperature());
+        }
+
         ESP_LOGI(TAG, "ACCEL_X= %0.2f", mpu6050_get_accel_x());
         ESP_LOGI(TAG, "ACCEL_Y= %0.2f", mpu6050_get_accel_y());
-        mqtt_publish_data (mpu6050_get_accel_x(), mpu6050_get_accel_y(), mpu6050_get_accel_z(),
-        mpu6050_get_gyro_x(), mpu6050_get_gyro_y(), mpu6050_get_gyro_z(), mpu6050_get_temperature());
+        
         vTaskDelay(3000);
     }
 }
